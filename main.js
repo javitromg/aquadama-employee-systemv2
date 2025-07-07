b66acfd009cc84536ef80b4f0ccef56f06c58f54
javitromg <89654571+javitromg@users.noreply.github.com>
2025-07-07 15:58:06 +0100

    Add files via upload

--- main.js
+++
@@ -1,4 +0,0 @@
-// main.js - Lógica JS principal del sistema Aquadama
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('app').innerText = 'Sistema Aquadama cargado correctamente.';
-});
